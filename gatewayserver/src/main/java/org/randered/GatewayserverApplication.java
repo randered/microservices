a5cdd032b847e87fca905102479dd5cddccb525d
Vasil Vasilev <vasil.yanev15@gmail.com>
2024-01-25 15:44:58 +0200

    added gateway impl

---
+++ gatewayserver/src/main/java/org/randered/GatewayserverApplication.java
@@ -0,0 +1,39 @@
+package org.randered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/vbank/accounts/**")
+                        .filters(f -> f.rewritePath("/vbank/accounts/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/vbank/loans/**")
+                        .filters(f -> f.rewritePath("/vbank/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/vbank/cards/**")
+                        .filters(f -> f.rewritePath("/vbank/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://CARDS")).build();
+    }
+
+
+}
